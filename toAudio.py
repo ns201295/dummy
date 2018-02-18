1992390039e9578387e688dfdbbc6d7d5390cbf2
A1521 <nitin.sharma@axtria.com>
2018-02-18 15:45:06 +0530

    git it

--- toAudio.py
+++ toAudio.py
@@ -1,9 +1,9 @@
 from moviepy.editor import VideoFileClip as vid 
 from glob import glob
 import numpy, pickle
-
 filenames = glob("*.mp4")
-
 for f in filenames:
 	name = f[:-3] + "mp3"
 	vid(f).audio.write_audiofile(name)
+
+print("Lets git it")
