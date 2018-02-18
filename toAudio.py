6a012006dd172e5ec09038edd53454f7b4a5ad16
ns201295 <nitin.stormborn@gmail.com>
2018-02-18 14:33:24 +0530

    revert toAudio

--- toAudio.py
+++ toAudio.py
@@ -1,9 +1,8 @@
-from moviepy.editor import VideoFileClip as video 
+from moviepy.editor import VideoFileClip as vid 
 from glob import glob
-import numpy
 
 filenames = glob("*.mp4")
 
 for f in filenames:
 	name = f[:-3] + "mp3"
-	video(f).audio.write_audiofile(name)
+	vid(f).audio.write_audiofile(name)
